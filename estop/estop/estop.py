df7d7ba5e665f0741ba5896280667a6a9287c12b
Ashley Gross <iqjob3@gmail.com>
2025-08-01 15:19:07 -0400

    Clean up using pylint

--- estop/estop/estop.py
+++ estop/estop/estop.py
@@ -1,29 +1,40 @@
+import random
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Bool
-import random
 
 class EstopPublisher(Node):
-    def __init__(self, estop_generator):
-        super().__init__('estop')
-        self.publisher_ = self.create_publisher(Bool, 'estop', 10)
-        timer_period = 1.0  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.estop_generator = estop_generator
+# Takes a generator which should return a boolean, and publishes the values 
+# provided to ROS2 topic "estop" with a period of 1 second
 
-    def timer_callback(self):
-        bool_msg = Bool()
-        data = next(self.estop_generator, None)
-        if data != None:
-            bool_msg.data = data     
-            self.publisher_.publish(bool_msg)
-            self.get_logger().info('Publishing estop: "%s"' % str(bool_msg.data))
-        else:
-            print("Data done")
+  def __init__(self, estop_generator):
+    super().__init__('estop')
+    self.publisher_ = self.create_publisher(Bool, 'estop', 10)
+    timer_period = 1.0  # seconds
+
+    # create timer that will call timer_callback every 1 second
+    self.timer = self.create_timer(timer_period, self.timer_callback)
+    self.estop_generator = estop_generator
+
+  def timer_callback(self):
+  # Uses generator provided from constructor to generate a new estop value and 
+  # publishes it to ROS2 topic "estop"
+
+    bool_msg = Bool()
+    data = next(self.estop_generator, None)
+    if data is not None:
+        bool_msg.data = data     
+        self.publisher_.publish(bool_msg)
+        self.get_logger().debug(f'Publishing estop: "{bool_msg.data}"')
+    else:
+        self.get_logger().warn("Data done")
+        print("Data done")
 
 def make_estop():
+    # This estop generate generates random estop values with a 20% probability.
     while True:
-        if random.randint(0, 10) == 0:
+        if random.randint(0, 10) < 2:
             yield True
         else:
             yield False
--- range_sensor/range_sensor/range_sensor.py
+++ range_sensor/range_sensor/range_sensor.py
@@ -1,27 +1,38 @@
+import random
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
-import random
 
 class RangePublisher(Node):
-    def __init__(self, range_generator):
-        super().__init__('range_sensor')
-        self.publisher_ = self.create_publisher(Int16, 'range', 10)
-        timer_period = 1.0  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.range_generator = range_generator
+  # Publishes range data produced by a generator every second to ROS2 topic
+  # "range"
 
-    def timer_callback(self):
-        msg = Int16()
-        data = next(self.range_generator, None)
-        if data != None:
-            msg.data = data
-            self.publisher_.publish(msg)
-            self.get_logger().info('Publishing: "%s"' % str(msg.data))
-        else:
-            print("Data done")
+  def __init__(self, range_generator):
+      # range_generator should be a generator that takes 0 arguments and
+      # produces 16 bit integers
+      super().__init__('range_sensor')
+      self.publisher_ = self.create_publisher(Int16, 'range', 10)
+
+      # create timer that will call timer_callback every 1 second
+      timer_period = 1.0  # seconds
+      self.timer = self.create_timer(timer_period, self.timer_callback)
+      self.range_generator = range_generator
+
+  def timer_callback(self):
+  # Uses generator provided from constructor to generate a new range value and 
+  # publishes it to ROS2 topic "range"
+    msg = Int16()
+    data = next(self.range_generator, None)
+    if data is not None:
+        msg.data = data
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing: "{msg.data}"')
+    else:
+        print("Data done")
 
 def make_range():
+    # returns a random range value between 0 and 1000
     while True:
         yield random.randint(0, 1000)
 
